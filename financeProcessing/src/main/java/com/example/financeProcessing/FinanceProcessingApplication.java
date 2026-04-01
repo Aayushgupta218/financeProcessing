b01f5839ca8549b67b6c5a5ef5d14d4f5ff3b59f
Aayushgupta218 <aggarwalaayush220@gmail.com>
2026-04-01 22:35:54 +0530

    first commit

---
+++ financeProcessing/src/main/java/com/example/financeProcessing/FinanceProcessingApplication.java
@@ -0,0 +1,13 @@
+package com.example.financeProcessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceProcessingApplication.class, args);
+	}
+
+}
---
+++ financeProcessing/src/test/java/com/example/financeProcessing/FinanceProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financeProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
